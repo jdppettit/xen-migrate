209aa560f104acdd76e69d116145c633127aec32
root <root@dev>
2014-04-03 21:45:25 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a simple script that will allow you to dd two disk images, grab a xen config and scp it over to
+a different host.
+
+The script does not yet snapshot the LV's for you, but I'll add that later. 
+
+Check --help for more info.
---
+++ xen-migrate.py
@@ -0,0 +1,68 @@
+import subprocess as s
+import argparse as a
+import sys
+
+parser = a.ArgumentParser(description='Migrate a Xen instance from one host to another.')
+parser.add_argument('diskpath', metavar='disk path', help='path to the domU\'s disk image')
+parser.add_argument('swappath', metavar='swap path', help='path to the domU\'s swap image')
+parser.add_argument('configpath', metavar='config path', help='path to the xen config file')
+parser.add_argument('targetuser', metavar='user', help='username on target system')
+parser.add_argument('targetip', metavar='ip', help='ip or hostname of target system')
+parser.add_argument('targetdir', metavar='dir', help='target directory')
+
+args = parser.parse_args()
+
+diskpath = args.diskpath
+swappath = args.swappath
+configpath = args.configpath
+targetuser = args.targetuser
+targetip = args.targetip
+targetdir = args.targetdir
+
+print "copying config from %s to /tmp" % configpath
+string = "cp %s /tmp/config.xen" % configpath
+
+try:
+	s.check_output(string,shell=True)
+except Exception, e:
+	print "Couldn't do it: %s" % e
+	sys.exit(0)
+
+string = "dd if=%s of=/tmp/tempdisk.xen" % diskpath
+print "dd from %s to /tmp, please wait" % diskpath
+
+try:
+	print s.check_output(string,shell=True)
+except Exception, e:
+	print "Couldn't do it: %s" % e
+	sys.exit(0)
+
+string = "dd if=%s of=/tmp/swapdisk.xen" % swappath
+print "dd from %s to /tmp, please wait" % swappath
+
+try:
+	print s.check_output(string,shell=True)
+except Exception, e:
+	print "Couldn't do it: %s" % e
+	sys.exit(0)
+
+string = "scp /tmp/*.xen %s@%s:%s" % (targetuser, targetip, targetdir)
+print "scping data from /tmp to %s please wait" % targetip
+
+try:
+	print s.check_output(string,shell=True)
+except Exception, e:
+	print "Couldn't do it: %s" % e
+	sys.exit(0)
+
+print "success, deleting temporary images"
+
+try:
+	s.check_output("rm /tmp/*.xen",shell=True)
+except Exception, e:
+	print "Couldn't do it: %s" % e
+	sys.exit(0)
+
+print "removed, done."
+
+
